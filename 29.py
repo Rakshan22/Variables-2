8423795ef6a4d7ef29bfb4daae3b191ca6242f0c
Rakshan22 <100563149+Rakshan22@users.noreply.github.com>
2022-07-25 16:49:25 +0530

    Add files via upload

---
+++ 29.py
@@ -0,0 +1,5 @@
+character_name = "Mike"
+print("He really liked the name " + character_name + ", ")
+print("But he didn't like being " + character_age + ".")
+phrase = "Giraffe Academy"
+print(phrase.replace("Giraffe", "Elephant"))
